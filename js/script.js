58b9f08ec233b4153d19d96528d4ad4337ab9576
J-tech0 <56737975+J-tech0@users.noreply.github.com>
2020-04-16 17:27:32 +0100

    added link to intern page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-const swup = new Swup(); // only this line when included with script tag
+const options = {
+  linkSelector:
+    'a[href^="' +
+    window.location.origin +
+    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])'
+};
+const swup = new Swup(options); // only this line when included with script tag
